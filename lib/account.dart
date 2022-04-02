b0491945a5562343d3872269310818027eaf21a0
Steven To <steven351906@yahoo.com.hk>
2022-04-02 18:33:06 +0800

    finished account

--- lib/account.dart
+++ lib/account.dart
@@ -10,7 +10,15 @@ class Account extends StatelessWidget {
       body: Center(
           child: Column(
         children: const [
-          Text('CrowdBuy Guys'),
+          SizedBox(width: 20, height: 40),
+          CircleAvatar(
+              child: Text(
+                'CG',
+                textScaleFactor: 2,
+              ),
+              radius: 40),
+          SizedBox(width: 20, height: 10),
+          Text('CrowdBuy Guys', style: TextStyle(fontSize: 20)),
           Text('Kwun Tong'),
           Divider(
             height: 20,
@@ -18,6 +26,32 @@ class Account extends StatelessWidget {
             indent: 0,
             endIndent: 0,
             color: Colors.grey,
+          ),
+          Text(
+            'Following',
+            style: TextStyle(fontSize: 18),
+          ),
+          Divider(
+            height: 20,
+            thickness: 1,
+            indent: 0,
+            endIndent: 0,
+            color: Colors.grey,
+          ),
+          Text(
+            'Liked Request',
+            style: TextStyle(fontSize: 18),
+          ),
+          Divider(
+            height: 20,
+            thickness: 1,
+            indent: 0,
+            endIndent: 0,
+            color: Colors.grey,
+          ),
+          Text(
+            'My Requests',
+            style: TextStyle(fontSize: 18),
           ),
         ],
       )),
