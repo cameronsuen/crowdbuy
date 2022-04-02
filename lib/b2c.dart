7476f4e7cb92332417b40fe12501134217f648e2
Steven To <steven351906@yahoo.com.hk>
2022-04-02 12:41:37 +0800

    b2c updated

--- lib/b2c.dart
+++ lib/b2c.dart
@@ -5,26 +5,38 @@ class B2C extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+        primary: const Color(0xFFFF7276),
+        textStyle: const TextStyle(fontSize: 20));
+
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Card(
           child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: const <Widget>[
-              ListTile(
-                // tileColor: Color.fromARGB(156, 255, 0, 0),
-                leading: Icon(Icons.album),
-                title: Text('Koey98'),
-                subtitle: Text('4.8'),
-              ),
-              ListTile(
-                subtitle: Text('4.8'),
-              ),
-            ],
+        children: [
+          const Image(
+            image: NetworkImage('../asset/discount_photo.jpg'),
           ),
-        ),
-      ),
+          const ListTile(
+            title: Text('ASport 40% off over \$500'),
+            subtitle: Text('Post by ASport Official'),
+          ),
+          const ListTile(
+            // tileColor: Color.fromARGB(156, 255, 0, 0),
+            leading: Icon(Icons.album),
+            title: Text('Koey98'),
+            subtitle: Text('4.8'),
+          ),
+          Row(children: [
+            ElevatedButton(
+              style: style,
+              onPressed: () {},
+              child: const Text('New Request'),
+            ),
+            const Icon(Icons.chat_bubble_outline_rounded)
+          ])
+        ],
+      )),
     );
   }
 }
